a9cef05bc15cfa06cc9ca08125aa2c3eb6e80c06
engswee <engswee@users.noreply.github.com>
2015-03-06 13:04:32 +0800

    Update and rename util to util/AbstractModuleConverter.java

---
+++ util/AbstractModuleConverter.java
@@ -0,0 +1,27 @@
+package com.equalize.xpi.af.modules.util;
+
+import com.sap.aii.af.lib.mp.module.ModuleException;
+import com.sap.engine.interfaces.messaging.api.Message;
+import com.sap.engine.interfaces.messaging.api.XMLPayload;
+
+public abstract class AbstractModuleConverter {
+	protected final Message msg;
+	protected final XMLPayload payload;
+	protected final AuditLogHelper audit;
+	protected final ParameterHelper param;
+	protected final DynamicConfigurationHelper dyncfg;
+	
+	public AbstractModuleConverter (Message msg, ParameterHelper param, AuditLogHelper audit, DynamicConfigurationHelper dyncfg) {
+		this.msg = msg;
+		this.payload = this.msg.getDocument();
+		this.param = param;
+		this.dyncfg = dyncfg;
+		this.audit = audit;						
+	}
+	
+	public abstract void retrieveModuleParameters() throws ModuleException;
+
+	public abstract void parseInput() throws ModuleException;
+
+	public abstract byte[] generateOutput() throws ModuleException;
+}
