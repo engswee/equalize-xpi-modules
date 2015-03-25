59561c4ef015fa57a335de178292d9ee0a19aab4
engswee <engswee@users.noreply.github.com>
2015-03-25 12:25:45 +0800

    Create FormatConversionBean.java

---
+++ FormatConversionBean.java
@@ -0,0 +1,20 @@
+package com.equalize.xpi.af.modules;
+
+import com.equalize.xpi.af.modules.util.AbstractModule;
+import com.equalize.xpi.af.modules.util.AbstractModuleConverter;
+import com.equalize.xpi.af.modules.util.ConverterFactory;
+import com.sap.aii.af.lib.mp.module.ModuleException;
+
+public class FormatConversionBean extends AbstractModule {
+	@Override
+	protected void processModule() throws ModuleException {
+		ConverterFactory factory = ConverterFactory.newInstance();
+		AbstractModuleConverter converter = factory.newConverter(this.msg, this.param, this.audit, this.dyncfg);
+		converter.retrieveModuleParameters();
+		converter.parseInput();
+		byte[] outputBytes = converter.generateOutput();
+
+		// Update payload content
+		changePayloadContent(outputBytes);
+	}
+}
