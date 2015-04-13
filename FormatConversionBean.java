c11a88e3f2c74367a91ec4e69afaf650ee0ce3b6
engswee <engswee@users.noreply.github.com>
2015-04-13 16:05:32 +0800

    Update FormatConversionBean.java

--- FormatConversionBean.java
+++ FormatConversionBean.java
@@ -9,7 +9,7 @@ public class FormatConversionBean extends AbstractModule {
 	@Override
 	protected void processModule() throws ModuleException {
 		ConverterFactory factory = ConverterFactory.newInstance();
-		AbstractModuleConverter converter = factory.newConverter(this.msg, this.param, this.audit, this.dyncfg);
+		AbstractModuleConverter converter = factory.newConverter(this.msg, this.param, this.audit, this.dyncfg, this.debug);
 		converter.retrieveModuleParameters();
 		converter.parseInput();
 		byte[] outputBytes = converter.generateOutput();
